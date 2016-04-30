57688eb633e4a3b8570a35cb374b7fbdf84797dd
KingPixil <simplecooldude1@gmail.com>
2016-04-30 12:08:05 -0700

    Update JARVIS.py

--- JARVIS.py
+++ JARVIS.py
@@ -3,9 +3,8 @@ import pigpio
 import pygame
 import time
 import math
+
 # Declare some variables
-enable_pin = 4
-enable_pin_2 = 23
 input_pin_1 = 22
 input_pin_2 = 17
 input_pin_3 = 18
@@ -20,8 +19,6 @@ VERSION = "0.2.0"
 direction = "C"
 pig = pigpio.pi()
 # All of the outputs
-pig.set_mode(enable_pin, pigpio.OUTPUT)
-pig.set_mode(enable_pin_2, pigpio.OUTPUT)
 pig.set_mode(input_pin_1, pigpio.OUTPUT)
 pig.set_mode(input_pin_2, pigpio.OUTPUT)
 pig.set_mode(input_pin_3, pigpio.OUTPUT)
@@ -37,24 +34,36 @@ pig.set_PWM_dutycycle(input_pin_2, dutycycle2)
 pig.set_PWM_dutycycle(input_pin_3, dutycycle1)
 pig.set_PWM_dutycycle(input_pin_4, dutycycle2)
 pygame.init()
-pygame.display.set_mode((300, 200))
+DISPLAYSURF = pygame.display.set_mode((400, 300))
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE  = (39, 174, 96)
 control = 0
 running = True
 print ("JARVIS VERSION " + VERSION + " HAS BEEN SETUP SUCCESSFULLY")
+pygame.display.set_caption("JARVIS")
+WINDOWWIDTH = 400
+WINDOWHEIGHT = 300
+windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
+windowSurface.fill(BLACK)
+DISPLAYSURF.fill(BLACK)
+fontObj = pygame.font.Font('freesansbold.ttf' , 32)
+textSurfaceObj = fontObj.render(direction + " JARVIS " + str(255 - speed), True, BLUE)
+textRectObj = textSurfaceObj.get_rect()
+textRectObj.center = (200, 150)
+DISPLAYSURF.blit(textSurfaceObj, textRectObj)
+pygame.display.update()
 try:
-    def checkSpeed(speed):
-        if speed >= 255:
-            print ("WARNING: JARVIS HAS REACHED MAX SPEED")
-            speed = 255
-            return 255
-        elif speed <= 0:
-            print ("WARNING: JARVIS IS AT MINIMUM SPEED")
-            speed = 0
-            return 0
-        else:
-            return speed
     while running:
-        checkSpeed(speed)
+        windowSurface.fill(BLACK)
+        DISPLAYSURF.fill(BLACK)
+        fontObj = pygame.font.Font('freesansbold.ttf' , 32)
+        textSurfaceObj = fontObj.render(direction + " JARVIS " + str(255 - speed) , True, BLUE)
+        textRectObj = textSurfaceObj.get_rect()
+        textRectObj.center = (200, 150)
+        DISPLAYSURF.blit(textSurfaceObj, textRectObj)
+        pygame.display.update()
         pig.set_PWM_dutycycle(22, dutycycle2) 
         pig.set_PWM_dutycycle(17, dutycycle1)
         pig.set_PWM_dutycycle(18, dutycycle2)
@@ -63,7 +72,7 @@ try:
         for event in events:
             # Loop For Controlling the Robot
             if (event.type == pygame.KEYDOWN):
-                print ("JARVIS RECIEVED THE COMMAND:")
+                #print ("JARVIS RECIEVED THE COMMAND:")
                 if event.key == pygame.K_SPACE:
                     # STOP JARVIS
                     direction="Q"
@@ -72,7 +81,7 @@ try:
                     direction="C"
                 elif event.key == pygame.K_r:
                     # REBOOT JARVIS
-                    direction="R"
+                    direction="R" 
                 elif event.key == pygame.K_a and pygame.K_i:
                     direction="AI"
                 elif event.key == pygame.K_o and pygame.K_p:
@@ -81,69 +90,54 @@ try:
                 elif event.key == pygame.K_UP:
                     # GO FORWARD
                     direction = "F"
-                    checkSpeed(speed)
                 elif event.key == pygame.K_DOWN:
                     # GO BACKWARDS
                     direction = "B"
-                    checkSpeed(speed)
                 elif event.key == pygame.K_z:
                     # SLOW DOWN
                     direction = "SD"
-                    checkSpeed(speed)
                 elif event.key == pygame.K_x:
                     # SPEED UP
                     direction = "SU"
-                    checkSpeed(speed)
                 elif event.key == pygame.K_LEFT:
                     # TURN LEFT
                     direction = "L"
-                    checkSpeed(speed)
                 elif event.key == pygame.K_RIGHT:
                     # TURN RIGHT
                     direction = "R"
-                    checkSpeed(speed)
                 elif event.key == pygame.K_a:
                     # DRIFT LEFT
                     direction = "DL"
-                    checkSpeed(speed)
                 elif event.key == pygame.K_d:
                     # DRIFT RIGHT
-                    checkSpeed(speed)
                     direction = "DR"
 
 
             # DETECTING DIRECTION AND TAKE ACTION
             if direction=="Q":
-                print ("STOPPING JARVIS")
+                #print ("STOPPING JARVIS")
                 pig.write(input_pin_1, pigpio.LOW)
                 pig.write(input_pin_2, pigpio.LOW)
                 pig.write(input_pin_3, pigpio.LOW)
                 pig.write(input_pin_4, pigpio.LOW)
                 running = False
             if direction=="C":
-                print ("CUTTING JARVIS MOTORS")
+                #print ("CUTTING JARVIS MOTORS")
                 pig.write(input_pin_1, pigpio.LOW)
                 pig.write(input_pin_2, pigpio.LOW)
                 pig.write(input_pin_3, pigpio.LOW)
                 pig.write(input_pin_4, pigpio.LOW)
-            if direction=="R":
-                print ("REBOOTING JARVIS")
-                pig.write(input_pin_1, pigpio.HIGH)
-                pig.write(input_pin_2, pigpio.HIGH)
-                pig.write(input_pin_3, pigpio.HIGH)
-                pig.write(input_pin_4, pigpio.HIGH)
-                running = True
             if direction=="OP":
-                print ("JARVIS IS GOING INTO OP MODE")
+                #print ("JARVIS IS GOING INTO OP MODE")
                 pig.write(input_pin_1, pigpio.HIGH)
                 pig.write(input_pin_2, pigpio.HIGH)
                 pig.write(input_pin_3, pigpio.HIGH)
                 pig.write(input_pin_4, pigpio.HIGH)
-                speed = 253
-                checkSpeed(speed)
+                speed = 255
+                dutycycle1 = 255
+                dutycycle2 = 255
             if direction=="F":
-                if speed < 255:
-                    accel += 1
+                if speed < 256:
                     dutycycle2=0
                     pig.write(input_pin_1, pigpio.HIGH)
                     pig.write(input_pin_2, pigpio.HIGH)
@@ -151,11 +145,9 @@ try:
                     pig.write(input_pin_4, pigpio.HIGH)
                     control=0
                     dutycycle1=speed
-                    checkSpeed(speed)
-                    print("JARVIS IS GOING FORWARDS")                  
+                    #print("JARVIS IS GOING FORWARDS")                  
             elif direction=="B":
                 if speed > 0:
-                    accel -= 1
                     pig.write(input_pin_1, pigpio.HIGH)
                     pig.write(input_pin_2, pigpio.HIGH)
                     pig.write(input_pin_3, pigpio.HIGH)
@@ -163,41 +155,39 @@ try:
                     dutycycle1=0
                     control=1
                     dutycycle2=speed
-                    checkSpeed(speed)
-                    print ("JARVIS IS GOING BACKWARDS")
+                    #print ("JARVIS IS GOING BACKWARDS")
             elif direction=="SU":
-                if dutycycle1 > 11 and control == 0:
-                    speed -= accel
-                    dutycycle1 = speed
-                if dutycycle2 > 11 and control == 1:
-                    speed -= accel
-                    dutycycle2 = speed
-                checkSpeed(speed)
-                print ("JARVIS IS SPEEDING UP")
+                if speed < 256:
+                    if dutycycle1 > 11 and control == 0:
+                        speed -= accel
+                        dutycycle1 = speed
+                    if dutycycle2 > 11 and control == 1:
+                        speed -= accel
+                        dutycycle2 = speed
+                #print ("JARVIS IS SPEEDING UP")
             elif direction=="SD":
-                if dutycycle1 < 244 and control == 0:
-                    speed += accel
-                    dutycycle1 = speed
-                if dutycycle2 < 244 and control == 1:
-                    speed += accel
-                    dutycycle2 = speed
-                checkSpeed(speed)
-                print ("JARVIS IS SLOWING DOWN")
+                if speed > 0:
+                    if dutycycle1 < 244 and control == 0:
+                        speed += accel
+                        dutycycle1 = speed
+                    if dutycycle2 < 244 and control == 1:
+                        speed += accel
+                        dutycycle2 = speed
+                #print ("JARVIS IS SLOWING DOWN")
             elif direction=="L":
                 pig.write(input_pin_1, pigpio.HIGH)
                 pig.write(input_pin_2, pigpio.HIGH)
                 pig.write(input_pin_3, pigpio.LOW)
                 pig.write(input_pin_4, pigpio.LOW)
                 control=0
-                checkSpeed(speed)
-                print ("JARVIS IS TURNING LEFT")
+                #print ("JARVIS IS TURNING LEFT")
             elif direction=="R":
                 pig.write(input_pin_1, pigpio.LOW)
                 pig.write(input_pin_2, pigpio.LOW)
                 pig.write(input_pin_3, pigpio.HIGH)
                 pig.write(input_pin_4, pigpio.HIGH)
                 control=0
-                print ("JARVIS IS TURNING RIGHT")
+                #print ("JARVIS IS TURNING RIGHT")
             elif direction=="DL":
                 if dutycycle1 < 244:
                     dutycycle2=0
@@ -207,9 +197,6 @@ try:
                     pig.write(input_pin_4, pigpio.HIGH)
                     control=0
                     dutycycle1=speed - DIFFERENCE
-                else:
-                    checkSpeed(speed)
-                print ("JARVIS IS DRIFTING LEFT")
             elif direction=="DR":
                 if dutycycle1 < 244:
                     dutycycle2=0
@@ -219,12 +206,8 @@ try:
                     pig.write(input_pin_4, pigpio.HIGH)
                     control=0
                     dutycycle1=speed + DIFFERENCE
-                else:
-                    checkSpeed(speed)
-                print ("JARVIS IS DRIFTING RIGHT")
 except KeyboardInterrupt:
     print("Quit")
-    pig.write(enable_pin, pigpio.LOW)
     pig.stop()
 
 # CHANGELOG
@@ -248,4 +231,6 @@ except KeyboardInterrupt:
         Take Out Enable Pins
             * Enable Pin's are not Needed
             * C Toggles instead of temporarily stopping
+            * Add GUI
+            * Remove Checkspeed (Doesn't Work)
 """
